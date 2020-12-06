50a73c43c4238a75831c9aeef2818fdf110d128d
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-12-06 16:33:35 -0300

    dev/prod yml

--- src/main/java/com/eduardonunes/bookstoremanager/config/SwaggerConfig.java
+++ src/main/java/com/eduardonunes/bookstoremanager/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.eduardonunes.bookstoremanager.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
