ed5fc338c365ba883796ac93dfc597a0ad5fafd6
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-12-15 14:51:21 -0300

    creation publisher dto

---
+++ src/main/java/com/eduardonunes/bookstoremanager/publishers/dto/PublisherDTO.java
@@ -0,0 +1,36 @@
+package com.eduardonunes.bookstoremanager.publishers.dto;
+
+import com.eduardonunes.bookstoremanager.books.entity.Book;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PublisherDTO {
+
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 255)
+    private String name;
+
+    @NotEmpty
+    @NotNull
+    @Size(max = 50)
+    private String code;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate foundationDate;
+
+
+}
