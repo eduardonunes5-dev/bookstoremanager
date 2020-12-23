4aa45c365f1ccc2a4a2adbb631041bc524368668
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2020-12-22 22:05:40 -0300

    created books dtos

---
+++ src/main/java/com/eduardonunes/bookstoremanager/books/dto/BookResponse.java
@@ -0,0 +1,35 @@
+package com.eduardonunes.bookstoremanager.books.dto;
+
+import com.eduardonunes.bookstoremanager.author.dto.AuthorDTO;
+import com.eduardonunes.bookstoremanager.publishers.dto.PublisherDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.ISBN;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String isbn;
+
+    private Long pages;
+
+    private Long chapters;
+
+    private AuthorDTO author;
+
+    private PublisherDTO publisher;
+
+
+}
